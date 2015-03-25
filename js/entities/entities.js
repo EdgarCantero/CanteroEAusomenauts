42cd309b181e353353afa976a3d24cec2ac14ca5
Edgar Cantero <cantero098@gmail.com>
2015-03-25 09:21:22 -0700

    Madee a new charavtor

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,8 +12,14 @@ game.PlayerEntity = me.Entity.extend({
 
             }]);
 
-        this.body.setVelocity(5, 20);
+        this.body.setVelocity(10, 20);
+        //keeps track of the players direction
         this.facing = "right";
+        
+        this.now = new Date().getTime();
+        this.lastHit = this.now;
+        this.lastAttack = new Date().getTime();
+        
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 
 
@@ -24,14 +30,15 @@ game.PlayerEntity = me.Entity.extend({
         this.renderable.setCurrentAnimation("idle");
     },
     update: function(delta) {
+        this.now = new Date().getTime();
         if (me.input.isKeyPressed("right")) {
             this.body.vel.x += this.body.accel.x * me.timer.tick;
-            this.body.facing = "right";
+            this.facing = "right";
             this.flipX(true);
 
         } else if (me.input.isKeyPressed('left')) {
             this.flipX(false);
-            this.body.facing= "left";
+            this.facing= "left";
             this.body.vel.x -= this.body.accel.x * me.timer.tick;
 
         } else {
@@ -54,11 +61,11 @@ game.PlayerEntity = me.Entity.extend({
             }
         }
 
-        else if (this.body.vel.x !== 0) {
+        else if (this.body.vel.x !== 0 && !this.renderable.isCurrentAnimation("attack")) {
             if (!this.renderable.isCurrentAnimation("walk")) {
                 this.renderable.setCurrentAnimation("walk");
             }
-        } else {
+        } else if (!this.renderable.isCurrentAnimation("attack")){
             this.renderable.setCurrentAnimation("idle");
         }
 
@@ -76,15 +83,23 @@ game.PlayerEntity = me.Entity.extend({
             var ydif = this.pos.y - response.b.pos.y;
             var xdif = this.pos.x - response.b.pos.x;
             
+            
+            
             if(ydif<-40 && xdif< 70 && xdif>-35){
                 this.body.falling = false;
                 this.body.vel.y = -1;
-            }else if(xdif>-35 && this.facing==='right' && (xdif<0)){
+            }else if(xdif>-34 && this.facing==='right' && (xdif<0)){
                 this.body.vel.x = 0;
                 this.pos.x = this.pos.x -1;
-            }else if(xdif<70 && this.facing==='left' && (xdif>0)){
+            }else if(xdif<35 && this.facing==='left' && (xdif>0)){
                 this.body.vel.x = 0;
                 this.pos.x = this.pos.x +1;
+            }
+            
+            if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 1000){
+                console.log("tower Hit");
+                this.lastHit = this.now;
+                response .b.loseHealth();
             }
             
         }
@@ -109,7 +124,7 @@ game.PlayerBaseEntity = me.Entity.extend({
         this.alwaysUpdate = true;
         this.body.onCollision = this.onCollision.bind(this);
 
-        this.type = "EnemyBaseEntity";
+        this.type = "PlayerBaseEntity";
 
         this.renderable.addAnimation("idle", [0]);
         this.renderable.addAnimation("broken", [1]);
@@ -170,6 +185,10 @@ game.EnemyBaseEntity = me.Entity.extend({
     },
     onCollision: function() {
 
+    },
+    
+    loseHealth: function(){
+        this.health--;
     }
 
 });
