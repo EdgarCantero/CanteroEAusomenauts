94bb9dfa05b47869c0b387729b9b7168e7ccb9c6
Edgar Cantero <cantero098@gmail.com>
2015-04-14 09:21:07 -0700

    mAD;.HB K.MF V; P] CV;.HIKGBsK;XFC'[ LF MKB.  ASDJ/L,N/ CXF J,N

--- js/game.js
+++ js/game.js
@@ -46,7 +46,9 @@ var game = {
         
         me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0});
 
-	// Initialize the audio.
+	me.state.SPENDEXP = 112;
+            
+        // Initialize the audio.
 	me.audio.init("mp3,ogg");
 
 	// Set a callback to run when loading is complete.
@@ -71,7 +73,7 @@ var game = {
             
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
-               // me.state.set(me.state.SPENDEXP, new game.SpendExp());
+                me.state.set(me.state.SPENDEXP, new game.SpendExp());
 
 		// Start the game.
 		me.state.change(me.state.MENU);
