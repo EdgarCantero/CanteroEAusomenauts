364702462fedb5aecc888724777d1051268915cb
Edgar Cantero <cantero098@gmail.com>
2015-04-29 09:21:39 -0700

    kbvuopobwuogbow

--- php/controller/login-user.php
+++ php/controller/login-user.php
@@ -1,27 +1,35 @@
 <?php
-    require_once(__DIR__ . "/../model/config.php");
-    
-    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
-    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-    $query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username= '$username'");
-    
-    if($query->num_rows == 1){
-        $row = $query->fetch_array();
-        
-        if($row["password"] === crypt($password, $row["salt"])) {
-            $_SEESSION["authenticated"] = true;
-            echo "<p><button><a href='post.php'>Create Post</a></button><button><a href='login.php' >Login</a></button>
-                <button><a href='http://localhost/Cantero-Blog/post.php'>Create Post</a></button>
-                <button><a href='index.php'>Blog</a></button>
-                <button><a href='http://localhost/Cantero-Blog/view/register-form.php'>Register</a></button>"
-              .  " <br> Login Succesful!</p>";
-        }
-        else {
-            echo "<p>Invalid username and password</p>";
-        }
-    }
-    else {
+require_once(__DIR__ . "/../model/config.php");
+
+$array = array(
+    'exp' => '',
+    'exp1' => '',
+    'exp2' => '',
+    'exp3' => '',
+    'exp4' => '',
+);
+
+$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+$query = $_SESSION["connection"]->query("SELECT FROM users WHERE username= '$username'");
+
+if ($query->num_rows == 1) {
+    $row = $query->fetch_array();
+
+    if ($row["password"] === crypt($password, $row["salt"])) {
+        $_SEESSION["authenticated"] = true;
+        $array["exp"] = $row["exp"];
+        $array["exp1"] = $row["exp1"];
+        $array["exp2"] = $row["exp2"];
+        $array["exp3"] = $row["exp3"];
+        $array["exp4"] = $row["exp4"];
+        $_SESSION["name"] = $username;
+          echo json_encode($array);
+    } else {
         echo "<p>Invalid username and password</p>";
     }
+} else {
+    echo "<p>Invalid username and password</p>";
+}
     
